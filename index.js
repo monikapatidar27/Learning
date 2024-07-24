645e7a2d1dd682668e14e5a9e10e4ac64e3c1726
Monika Patidar <monikapatidar33440@gmail.com>
2024-07-24 18:43:08 +0530

    implement basic concept

--- index.js
+++ index.js
@@ -91,16 +91,82 @@
 // })();
 
 
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
  
-const port = 3000;
-app.get("/", (req, res) => {
-    res.send(`<div>
-    <h2>welcomw to the page</h2>
+// const port = 3000;
+// app.get("/", (req, res) => {
+//     res.send(`<div>
+//     <h2>welcomw to the page</h2>
     
-  </div>`);
+//   </div>`);
+// });
+// app.listen(port, () => {
+//     console.log(`Listening to port ${port}`);
+// });
+
+// const getSum = (num1, num2) => {
+//   const myPromise = new Promise((resolve, reject) => {
+//     if (!isNaN(num1) && !isNaN(num2)) {
+//       resolve(num1 + num2);
+//     } else {
+//       reject(new Error("Not a valid number"));
+//     }
+//   });
+
+//   return myPromise;
+// }
+
+// console.log(getSum(10, 20));
+
+// const express = require("express");
+// const app = express();
+// const axios = require("axios");
+
+// app.post("/user", async (req, res) => {
+//   try {
+//     const payload = { name: "Aashita Iyer", email: "aashita.iyer@email.com" };
+//     const response = await axios.post("http://httpbin.org/post", payload);
+//     console.log(response.data);
+//     res.status(200).json(response.data);
+//   } catch (err) {
+//     res.status(500).json({ message: err });
+//   }
+// });
+
+// app.listen(3000, function () {
+//   console.log(`App listening at http://localhost:3000/`);
+// });
+
+// const fs = require('fs');
+// const util = require('util');
+
+// const readFile = util.promisify(fs.readFile);
+
+// const read = async () => {
+//   try {
+//     const result = await readFile('file.txt');
+//     console.log(result);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// read();
+
+const express = require("express");
+const logger = require("./logger")
+const app = express();
+
+app.get("/event", (req, res, next) => {
+  try {
+    throw new Error("Not User!");
+  } catch (error) {
+    logger.error("Events Error: Unauthenticated user");
+    res.status(500).send("Error!");
+  }
 });
-app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
+
+app.listen(3000, () => {
+  logger.info("Server Listening On Port 3000");
 });
